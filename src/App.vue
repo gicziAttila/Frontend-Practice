e9af82e2b795f43c90a5a2287e1d4bbdd060cf5e
Giczi Attila <giczi.attila.istvan@gmail.com>
2026-01-05 20:10:59 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+//1. Adatmodell (class/interface)$$
+import type { Student } from './classes/Student';
+import StudentCard from './components/StudentCard.vue';
+//Lista létrehozása
+const students = ref<Student[]>([
+    { id: 1, name: "Kiss Béla", isActive: true },
+    { id: 2, name: "Nagy Anna", isActive: false },
+    { id: 3, name: "Kovács János", isActive: true },
+])
+//Függvény
+const deleteStudents = (id: number) => {
+    students.value = students.value.filter(s => s.id !== id);
+}
+</script>
+<template>
+    <div class="max-w-2xl mx-auto mt-10 p-5 bg-gray-50 min-h-screen font-sans">
+        <h1 class="text-3xl font-bold text-gray-800 mb-6 text-center">Tanulók nyilvántartása</h1>
+        <div class="space-y-4">
+            <StudentCard v-for="s in students" :key="s.id" :student="s" @delete="deleteStudents" />
+        </div>
+        <p v-if="students.length === 0" class="text-center text-gray-500 mt-8 italic">
+            Nincs több tanuló a listában.
+        </p>
+    </div>
+</template>
