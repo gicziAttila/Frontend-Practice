421838a99bbb8af24b2985cd54be431322528567
Giczi Attila <giczi.attila.istvan@gmail.com>
2026-01-14 13:16:09 +0100

    feat: implementing api fetching with error handling

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 //1. Adatmodell (class/interface)$$
 import type { Student } from './classes/Student';
 import StudentCard from './components/StudentCard.vue';
 import StudentForm from './components/StudentForm.vue';
 //Lista létrehozása
-const students = ref<Student[]>([
-    { id: 1, name: "Kiss Béla", isActive: true },
-    { id: 2, name: "Nagy Anna", isActive: false },
-    { id: 3, name: "Kovács János", isActive: true },
-])
+const students = ref<Student[]>([]);
+const isLoading = ref(false);
+const error = ref<string | null>(null);
+
+const fetchStudents = async () => {
+    isLoading.value = true;
+    error.value = null;
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) throw new Error('Hiba történt berültéskor!');
+        const data = await response.json();
+        students.value = data.map((user: any) => ({
+            id: user.id,
+            name: user.name,
+            isActive: Math.random() < 0.5
+        }));
+    } catch (err: any) {
+        error.value = 'Nem sikerült betölteni az adatokat: ' + err.message;
+    } finally {
+        isLoading.value = false;
+    }
+};
+onMounted(() => {
+    fetchStudents();
+});
 //Függvény
 const deleteStudents = (id: number) => {
     students.value = students.value.filter(s => s.id !== id);
@@ -27,7 +47,15 @@ const addStudent = (name: string, isActive: boolean) => {
 <template>
     <div class="max-w-2xl mx-auto mt-10 p-5 bg-gray-50 min-h-screen font-sans">
         <h1 class="text-3xl font-bold text-gray-800 mb-6 text-center">Tanulók nyilvántartása</h1>
-        <div class="mb-8">
+        <div v-if="isLoading" class="text-center py-10">
+            <span class="text-blue-600 text-lg animate-pulse">Adatok betöltése...</span>
+        </div>
+        <div v-else-if="error">
+            <p class="text-center text-red-500 mt-8 italic">A betöltés során hiba lépett fel</p>
+            {{ error }}
+            <button @click="fetchStudents" class="block mx-auto mt-2 underline">Újrapróbálom</button>
+        </div>
+        <div class="mb-8" v-else>
             <StudentForm @add="addStudent" />
         </div>
         <div class="space-y-4">
